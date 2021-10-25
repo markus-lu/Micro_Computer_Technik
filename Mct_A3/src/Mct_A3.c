8f13b8b1f3eeae07c638ed6b4984e1cae9df9198
simon <simon@simonscode.org>
2021-10-25 15:04:06 +0200

    Add doxygen comments

--- Mct_A3/src/Mct_A3.c
+++ Mct_A3/src/Mct_A3.c
@@ -1,7 +1,7 @@
 /*
  ===============================================================================
  Name        : Mct_A3.c
- Author      : $(author)
+ Author      : Markus Luckau, Simon Struck
  Version     :
  Copyright   : $(copyright)
  Description : main definition
@@ -9,7 +9,9 @@
  */
 
 #ifdef __USE_CMSIS
+
 #include "LPC17xx.h"
+
 #endif
 
 #include <cr_section_macros.h>
@@ -17,120 +19,282 @@
 #include <stdio.h>
 #include "Mct_A3.h"
 
+/**
+ * // TODO: äöüß <=========================================================================================
+ *  \file     Mct_A3.c
+*/
+
+/**
+ *  \mainpage A3: Rechenmaschine
+ *
+ *  Dies ist usere Lösung zu Aufgabe A3: Rechenmaschine.<BR><BR>
+ *  Dieses Programm multipliziert zwei Zahlen miteinander,
+ *  die über Schalter binär angegeben werden.
+ *
+ *  <HR>
+ *
+ *  \section sec1 Aufgabenstellung
+ *  Programmierung einer einfachen Rechenmaschine in der Programmiersprache C
+ *  auf dem Mikrocontroller-Entwicklungssystem MCTlite.
+ * Nach dem Programmstart soll zunächst mittels des Tasters TA1 eine an den
+ * Eingängen E0 .. E3 angelegte Binärzahl als Multiplikator A eingelesen werden.
+ * Danach wird Multiplikand B an E0 .. E3 angelegt und
+ * über den Taster TA2 eingelesen.<BR>
+ * Im Anschluss daran wird das Produkt C = A * B gebildet und
+ * auf den 8 gelben LEDs ausgegeben.<BR>
+ * Diese LEDs können, optional, durch den Taster TA3 gelöscht werden.
+ * Danach beginnt das Programm wieder von vorn.<BR>
+ *
+ *  \section sec2 Unterprogramme
+ *
+ *  \callgraph
+ *
+ *  \subsection sec2_1 Allgemeines, Initialisierung
+ *
+ *  Hier werden die C-Funktionen beschrieben, die der Initialisierung
+ *  der Hardware sowie weiteren, einmalig auszuführenden Aufgaben
+ *  dienen ("Init").
+ *
+ *  \subsection sec2_2 Ablaufsteuerung
+ *
+ *  Hier erfolgt die Beschreibung der eigentlichen Ablaufsteuerung
+ *  des Programms ("Loop").
+ *
+ *  <HR>
+ *
+ *   \author	Markus Luckau, Simon Struck
+ *   \date		25.10.2021
+ *   \version 	1.0             Fertige Version
+ */
+
+
+/*********************************************************************/
+/**
+ *
+ * \brief Dies ist das Hauptprogramm
+
+Dieses Programm multipliziert zwei Zahlen miteinander, die über Schalter Binär angegeben werden
+
+\param   -
+
+\return  wird nie erreicht
+
+\version 25.10.2021
+
+\todo    -
+\bug     keine Fehler bekannt
+**********************************************************************/
 int main(void) {
-	uint32_t keys = 0; // Tastencodes
-	uint32_t Faktor1 = 0; // Multiplikator
-	uint32_t Faktor2 = 0; // Multiplikand
-	uint32_t Produkt = 0; // Produkt der Multiplikation
-	uint32_t valuesAvailable = A_B_NOT_AVAILABLE; // welche Werte wurden eingelesen ?
+    uint32_t keys = 0; // Tastencodes
+    uint32_t Faktor1 = 0; // Multiplikator
+    uint32_t Faktor2 = 0; // Multiplikand
+    uint32_t Produkt = 0; // Produkt der Multiplikation
+    uint32_t valuesAvailable = A_B_NOT_AVAILABLE; // welche Werte wurden eingelesen ?
 
-	io_init(); // I/O-Ports initialisieren
-	show_result(Produkt); // anfangs alle LEDs aus
+    io_init(); // I/O-Ports initialisieren
+    show_result(Produkt); // anfangs alle LEDs aus
 
-	while (1) {
-		keys = getkey(); // Taster auslesen
+    while (1) {
+        keys = getkey(); // Taster auslesen
 
-		keyhandler(keys, &Faktor1, &Faktor2, &Produkt, &valuesAvailable); // einlesen + berechnen
+        keyhandler(keys, &Faktor1, &Faktor2, &Produkt, &valuesAvailable); // einlesen + berechnen
 
-		if ((valuesAvailable == A_B_AVAILABLE)
-				|| (valuesAvailable == A_B_NOT_AVAILABLE)) {
-			show_result(Produkt); // Ergebnis anzeigen
-		}
-	}
-	return 0;
+        if ((valuesAvailable == A_B_AVAILABLE)
+            || (valuesAvailable == A_B_NOT_AVAILABLE)) {
+            show_result(Produkt); // Ergebnis anzeigen
+        }
+    }
+    return 0;
 }
 
+
+/*********************************************************************/
+/**
+Funktion zur Initialisierung des verwendeten GPIO-Ports
+
+\param   -
+
+\return  -
+
+\version 25.10.2021
+
+\todo -
+\bug  keine Fehler bekannt
+**********************************************************************/
 void io_init(void) {
 
-	// Eingang Pins
+    // Eingang Pins
 
-	// Nutze als GPIO
-	LPC_PINCON->PINSEL0 = 0;
-	LPC_PINCON->PINSEL1 = 0;
+    // Nutze als GPIO
+    LPC_PINCON->PINSEL0 = 0;
+    LPC_PINCON->PINSEL1 = 0;
 
-	// Nutze als Eingang
-	LPC_PINCON->PINMODE0 = 0;
-	LPC_PINCON->PINMODE1 = 0;
+    // Nutze als Eingang
+    LPC_PINCON->PINMODE0 = 0;
+    LPC_PINCON->PINMODE1 = 0;
 
-	// Nutze nur einige Pins
-	LPC_GPIO0->FIOMASK = ~ALL_INPUTS;
+    // Nutze nur einige Pins
+    LPC_GPIO0->FIOMASK = ~ALL_INPUTS;
 
-	// Pins als Eingang
-	LPC_GPIO0->FIODIR = ~ALL_INPUTS;
+    // Pins als Eingang
+    LPC_GPIO0->FIODIR = ~ALL_INPUTS;
 
-	// Ausgangs Pins
+    // Ausgangs Pins
 
-	// Nutze als GPIO
-	LPC_PINCON->PINSEL4 = 0;
+    // Nutze als GPIO
+    LPC_PINCON->PINSEL4 = 0;
 
-	// Nutze nur Leds
-	LPC_GPIO2->FIOMASK = ~LEDS;
+    // Nutze nur Leds
+    LPC_GPIO2->FIOMASK = ~LEDS;
 
-	// Pins als Ausgang
-	LPC_GPIO2->FIODIR = LEDS;
+    // Pins als Ausgang
+    LPC_GPIO2->FIODIR = LEDS;
 
 }
 
+
+/*********************************************************************/
+/**
+Funktion zum anzeigen des Rechenergebnisses mitels Output LEDs
+
+\param   res
+		 Diese Variable beinhaltet das Rechenergebnis
+		 (0x0 - 0xFF)
+
+\return  -
+
+\version 25.10.2021
+
+\todo    -
+\bug     keine Fehler bekannt
+**********************************************************************/
 void show_result(uint32_t res) {
-
-	LPC_GPIO2->FIOPIN = ~(res << 1);
-
+    LPC_GPIO2->FIOPIN = ~(res << 1);
 }
 
+
+/*********************************************************************/
+/**
+Funktion zum Einlesen der gedrückten  Schalter und Taster
+
+\param   -
+
+\return  output
+         beinhaltet eine Bitreihenfplge für alle Schalter und Taster
+         (0x0 - 0x7F)
+
+\version 25.10.2021
+
+\todo    -
+\bug     keine Fehler bekannt
+**********************************************************************/
 uint32_t getkey(void) {
 
-	uint32_t input = ~LPC_GPIO0->FIOPIN;
+    uint32_t input = ~LPC_GPIO0->FIOPIN;
 
-	//printf("Input: %X \n", input);
+    //printf("Input: %X \n", input);
 
-	// Bits die graucht werden
-	// TA1 -> P0.6
-	// Ta2 -> P0.7
-	// Ta3 -> P0.8
-	// E0 -> P0.2
-	// E1 -> P0.3
-	// E2 -> P0.21
-	// E3 -> P0.16
+    // Bits die graucht werden
+    // TA1 -> P0.6
+    // Ta2 -> P0.7
+    // Ta3 -> P0.8
+    // E0 -> P0.2
+    // E1 -> P0.3
+    // E2 -> P0.21
+    // E3 -> P0.16
 
-	uint32_t buttons = (input & (TA1 | TA2 | TA3)) >> 6;
+    uint32_t buttons = (input & (TA1 | TA2 | TA3)) >> 6;
 
-	uint32_t switches = 0;
-	switches |= (input & E0) << 1;
-	switches |= (input & E1) << 1;
-	switches |= (input & E2) >> 16;
-	switches |= (input & E3) >> 10;
-	uint32_t output = switches | buttons;
+    uint32_t switches = 0;
+    switches |= (input & E0) << 1;
+    switches |= (input & E1) << 1;
+    switches |= (input & E2) >> 16;
+    switches |= (input & E3) >> 10;
+    uint32_t output = switches | buttons;
 
-	//printf("Output: %X \n", output);
-	return output;
+    //printf("Output: %X \n", output);
+    return output;
 
 }
+
+
+/*********************************************************************/
+/**
+Funktion zur Erzeugung einer variablen Verzögerungszeit
+
+\param   dtime
+		 Diese Variable legt die Verzögerungszeit fest
+
+\return  -
+
+\version 25.10.2021
+
+\todo    -
+\bug     keine Fehler bekannt
+**********************************************************************/
 void delay(uint32_t dtime) {
-
+    volatile uint32_t i;
+    for (i = 0; i < dtime; i++) {
+        __asm("nop");
+    }
 }
 
+
+/*********************************************************************/
+/**
+Funktion zum Speichern der Multiplikanten und Berechnen des Produkts
+
+\param   A
+		 Hier wird der erste Multiplikant gespeichert,
+		 sobald der Taster gedrueckt wird.
+		 (0 - 0xF)
+
+\param   B
+		 Hier wird der zweite Multiplikant gespeichert,
+		 sobald der Taster gedrueckt wird.
+		 (0 - 0xF)
+
+\param   C
+		 Hier wird das Produkt gespeichert,
+		 sobald beide Multiplikanten gespeichert wurden.
+		 (0 - 0xFF)
+
+\param   valuesAvailable
+		 Hier wird eine 2-Bit Flagge gespeichert,
+		 die anzeigt, welche Multiplikanten schon gespeichert wurden.
+		 (Bit 0 gesetzt: Multiplikant A gespeichert,
+		  Bit 1 gesetzt: Multiplikant B gespeichert)
+
+
+\return  -
+
+\version 25.10.2021
+
+\todo    -
+\bug     keine Fehler bekannt
+**********************************************************************/
 void keyhandler(uint32_t keys, uint32_t *A, uint32_t *B, uint32_t *C,
-		uint32_t *valuesAvailable) {
+                uint32_t *valuesAvailable) {
 
-	uint32_t input = keys >> 3;
-	if (keys & 1) {
-		*A = input;
-		*valuesAvailable |= A_AVAILABLE;
+    uint32_t input = keys >> 3;
+    if (keys & 1) {
+        *A = input;
+        *valuesAvailable |= A_AVAILABLE;
 
-	} else if (keys & 2) {
-		*B = input;
-		*valuesAvailable |= B_AVAILABLE;
+    } else if (keys & 2) {
+        *B = input;
+        *valuesAvailable |= B_AVAILABLE;
 
-	} else if (keys & 4) {
-		*A = 0;
-		*B = 0;
-		*C = 0;
-		*valuesAvailable = A_B_NOT_AVAILABLE;
-	}
+    } else if (keys & 4) {
+        *A = 0;
+        *B = 0;
+        *C = 0;
+        *valuesAvailable = A_B_NOT_AVAILABLE;
+    }
 
-	if (*valuesAvailable == A_B_AVAILABLE) {
-		*C = *A * *B;
-	}
+    if (*valuesAvailable == A_B_AVAILABLE) {
+        *C = *A * *B;
+    }
 
 }
 
