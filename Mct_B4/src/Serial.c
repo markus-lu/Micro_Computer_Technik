95d8290df2d0eabdaad02fc9048e546b25458e7c
Simon Struck <simon@simonscode.org>
2021-11-12 01:37:00 +0100

    Add timing constants for serial protocol

--- Mct_B4/src/Serial.c
+++ Mct_B4/src/Serial.c
@@ -1,9 +1,29 @@
+#include <Timer.h>
 #include "Serial.h"
+
+#define RISE_TIME 2                //   2 us
+#define FALL_TIME 120              // 120 us
+#define DOUBLE_CLOCK (1000 * 1000) //  1 MHz
+#define CLOCK (500 * 1000)         // 500 kHz
+
+#define MIN_CLOCK_PULSE_WIDTH   400 // ns
+#define MIN_STROBE_PULSE_WIDTH 1000 // ns
+#define MIN_DATA_SETUP_TIME     100 // ns
+#define MIN_DATA_HOLD_TIME      100 // ns
+#define MIN_CLK_STB_TIME       1000 // ns
+#define MIN_WAIT_TIME          1000 // ns
+
 
 static void init() {
     GPIO.init_pin(&Serial.clk);
     GPIO.init_pin(&Serial.dio);
     GPIO.init_pin(&Serial.stb);
+
+    GPIO.set_high(&Serial.stb);
+    GPIO.set_high(&Serial.clk);
+
+    Timer.init_timer2();
+    Timer.set_prescaler(LPC_TIM2, SystemCoreClock / DOUBLE_CLOCK);
 }
 
 static void write(uint8_t address, uint8_t *data, uint32_t length) {
