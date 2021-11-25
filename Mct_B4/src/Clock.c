8c27b48b6dff94e6b8e805b760744197ba2e95c8
Simon Struck <simon@simonscode.org>
2021-11-25 20:18:19 +0100

    Fix Clock menu

--- Mct_B4/src/Clock.c
+++ Mct_B4/src/Clock.c
@@ -2,75 +2,15 @@
 #include "Clock.h"
 #include "LEDKey.h"
 
-// TODO: Find correct values for these!
-#define BUTTON_EDIT 0
-#define BUTTON_DOWN 1
-#define BUTTON_UP 2
-#define BUTTON_BRIGHTNESS_DOWN 3
-#define BUTTON_BRIGHTNESS_UP 4
-
-void init() {
+void init(struct State *state) {
     LEDKey.init();
     RTC.init();
+    LEDKey.set_brightness(state->clock_brightness, true);
 }
 
-static const uint8_t SEGMENT_DIGITS[] = {SEG_0, SEG_1, SEG_2, SEG_3, SEG_4, SEG_5, SEG_6, SEG_7, SEG_8, SEG_9, SEG_DOT};
 
-
-static void draw_time(struct State *state) {
-    uint8_t segments[8];
-    if (state->clock_show_time) {
-    	segments[0] = 0;
-    	segments[1] = 0;
-        segments[2] = SEGMENT_DIGITS[state->time.hours / 10];
-        segments[3] = SEGMENT_DIGITS[state->time.hours % 10] | SEG_DOT;
-        segments[4] = SEGMENT_DIGITS[state->time.minutes / 10];
-        segments[5] = SEGMENT_DIGITS[state->time.minutes % 10] | SEG_DOT;
-        segments[6] = SEGMENT_DIGITS[state->time.seconds / 10];
-        segments[7] = SEGMENT_DIGITS[state->time.seconds % 10];
-        if (state->blink) {
-            segments[state->clock_selected_field * 2 + 2] = 0;
-            segments[state->clock_selected_field * 2 + 3] = 0 | SEG_DOT;
-        }
-    } else {
-        segments[0] = SEGMENT_DIGITS[state->time.day / 10];
-        segments[1] = SEGMENT_DIGITS[state->time.day % 10] | SEG_DOT;
-        segments[2] = SEGMENT_DIGITS[state->time.month / 10];
-        segments[3] = SEGMENT_DIGITS[state->time.month % 10] | SEG_DOT;
-        if (state->time.century) {
-            segments[4] = SEGMENT_DIGITS[2];
-            segments[5] = SEGMENT_DIGITS[0];
-        } else {
-            segments[4] = SEGMENT_DIGITS[1];
-            segments[5] = SEGMENT_DIGITS[9];
-        }
-        segments[6] = SEGMENT_DIGITS[state->time.year / 10];
-        segments[7] = SEGMENT_DIGITS[state->time.year % 10];
-        if (state->blink) {
-            switch (state->clock_selected_field) {
-                case 3:
-                    segments[0] = 0;
-                    segments[1] = 0 | SEG_DOT;
-                    break;
-                case 4:
-                    segments[2] = 0;
-                    segments[3] = 0 | SEG_DOT;
-                    break;
-                case 5:
-                    segments[4] = 0;
-                    segments[5] = 0;
-                    segments[6] = 0;
-                    segments[7] = 0;
-                    break;
-            }
-        }
-    }
-
-    LEDKey.set_display_data((state->blink && state->clock_selected_field == 6) ? 0 : state->time.weekday, segments);
-}
-
-#define DEC(value, max) if (state->time.value > 0) { state->time.value--; } else { state->time.value = (max); } break;
-#define INC(value, max) if (state->time.value < (max)) { state->time.value++; }else{state->time.value=(0);} break;
+#define DEC(value, max) if (state->time.value > 1) { state->time.value--; } else { state->time.value = (max); } break;
+#define INC(value, max) if (state->time.value < (max)) { state->time.value++; }else{ state->time.value=(0); } break;
 
 static void check_keypress(struct State *state) {
     bool should_redraw = state->clock_should_redraw;
@@ -78,19 +18,58 @@ static void check_keypress(struct State *state) {
     if (buttons != state->clock_last_buttons) {
         state->clock_should_redraw = true;
         switch (buttons) {
+            case BUTTON_TOGGLE_TIME_DATE:
+                state->clock_show_time = !state->clock_show_time;
+                break;
             case BUTTON_EDIT:
-                state->clock_edit_mode = !state->clock_edit_mode;
-                state->clock_selected_field = 0;
-                state->blink = true;
+                if (!state->clock_edit_mode) {
+                    state->clock_edit_mode = true;
+                    state->clock_selected_field = SELECTED_HOUR;
+                    state->blink = true;
+                } else {
+                    state->clock_edit_mode = false;
+                    state->blink = false;
+                    RTC.write_time(&state->time);
+                }
+                break;
+            case BUTTON_PREVIOUS:
+                if (state->clock_selected_field != SELECTED_HOUR) {
+                    state->clock_selected_field >>= 1;
+                } else {
+                    state->clock_selected_field = SELECTED_WEEKDAY;
+                }
+                if (state->clock_selected_field & SELECTED_TIME) {
+                    state->clock_show_time = true;
+                } else {
+                    state->clock_show_time = false;
+                }
+                break;
+            case BUTTON_NEXT:
+                if (state->clock_selected_field != SELECTED_WEEKDAY) {
+                    state->clock_selected_field <<= 1;
+                } else {
+                    state->clock_selected_field = SELECTED_HOUR;
+                }
+
+                if (state->clock_selected_field & SELECTED_TIME) {
+                    state->clock_show_time = true;
+                } else {
+                    state->clock_show_time = false;
+                }
                 break;
             case BUTTON_DOWN:
                 switch (state->clock_selected_field) {
-                    case 0: DEC(hours, 23)
-                    case 1: DEC(minutes, 59)
-                    case 2: DEC(seconds, 59)
-                    case 3: DEC(day, 31)
-                    case 4: DEC(month,12)
-                    case 5:
+                    case SELECTED_HOUR:
+                    DEC(hours, 23)
+                    case SELECTED_MINUTE:
+                    DEC(minutes, 59)
+                    case SELECTED_SECOND:
+                    DEC(seconds, 59)
+                    case SELECTED_DAY:
+                    DEC(day, 31)
+                    case SELECTED_MONTH:
+                    DEC(month, 12)
+                    case SELECTED_YEAR:
                         if (state->time.year > 0) {
                             state->time.year--;
                         } else {
@@ -98,7 +77,7 @@ static void check_keypress(struct State *state) {
                             state->time.century = !state->time.century;
                         }
                         break;
-                    case 6:
+                    case SELECTED_WEEKDAY:
                         if (state->time.weekday != Monday) {
                             state->time.weekday <<= 1;
                         } else {
@@ -109,19 +88,25 @@ static void check_keypress(struct State *state) {
                 break;
             case BUTTON_UP:
                 switch (state->clock_selected_field) {
-                    case 0: INC(hours, 24)
-                    case 1: INC(minutes, 60)
-                    case 2: INC(seconds, 60)
-                    case 3: INC(day, 31)
-                    case 4: INC(month,12)
-                    case 5:
+                    case SELECTED_HOUR:
+                    INC(hours, 24)
+                    case SELECTED_MINUTE:
+                    INC(minutes, 60)
+                    case SELECTED_SECOND:
+                    INC(seconds, 60)
+                    case SELECTED_DAY:
+                    INC(day, 31)
+                    case SELECTED_MONTH:
+                    INC(month, 12)
+                    case SELECTED_YEAR:
                         if (state->time.year < 99) {
                             state->time.year++;
                         } else {
                             state->time.year = 0;
                             state->time.century = !state->time.century;
                         }
-                    case 6:
+                        break;
+                    case SELECTED_WEEKDAY:
                         if (state->time.weekday != Sunday) {
                             state->time.weekday >>= 1;
                         } else {
@@ -129,14 +114,18 @@ static void check_keypress(struct State *state) {
                         }
                         break;
                 }
-
                 break;
             case BUTTON_BRIGHTNESS_DOWN:
                 state->clock_should_redraw = false;
-                if (state->clock_brightness > 0) {
+                if (state->clock_brightness > -1) {
                     state->clock_brightness--;
                 }
-                LEDKey.set_brightness(state->clock_brightness, true);
+
+                if (state->clock_brightness > -1) {
+                    LEDKey.set_brightness(state->clock_brightness, true);
+                } else {
+                    LEDKey.set_brightness(0, false);
+                }
                 break;
             case BUTTON_BRIGHTNESS_UP:
                 state->clock_should_redraw = false;
@@ -151,6 +140,77 @@ static void check_keypress(struct State *state) {
         }
         state->clock_last_buttons = buttons;
     }
+}
+
+
+static const uint8_t SEGMENT_DIGITS[] = {SEG_0, SEG_1, SEG_2, SEG_3, SEG_4, SEG_5, SEG_6, SEG_7, SEG_8, SEG_9, SEG_DOT};
+
+static void draw_time(struct State *state) {
+    uint8_t segments[8];
+    if (state->clock_show_time) {
+        segments[0] = 0;
+        segments[1] = 0;
+        segments[2] = SEGMENT_DIGITS[state->time.hours / 10];
+        segments[3] = SEGMENT_DIGITS[state->time.hours % 10] | SEG_DOT;
+        segments[4] = SEGMENT_DIGITS[state->time.minutes / 10];
+        segments[5] = SEGMENT_DIGITS[state->time.minutes % 10] | SEG_DOT;
+        segments[6] = SEGMENT_DIGITS[state->time.seconds / 10];
+        segments[7] = SEGMENT_DIGITS[state->time.seconds % 10];
+        if (state->blink) {
+            switch (state->clock_selected_field) {
+                case SELECTED_HOUR:
+                    segments[2] = 0;
+                    segments[3] = 0 | SEG_DOT;
+                    break;
+                case SELECTED_MINUTE:
+                    segments[4] = 0;
+                    segments[5] = 0 | SEG_DOT;
+                    break;
+                case SELECTED_SECOND:
+                    segments[6] = 0;
+                    segments[7] = 0;
+                    break;
+            }
+        }
+    } else {
+        segments[0] = SEGMENT_DIGITS[state->time.day / 10];
+        segments[1] = SEGMENT_DIGITS[state->time.day % 10] | SEG_DOT;
+        segments[2] = SEGMENT_DIGITS[state->time.month / 10];
+        segments[3] = SEGMENT_DIGITS[state->time.month % 10] | SEG_DOT;
+        if (state->time.century) {
+            segments[4] = SEGMENT_DIGITS[2];
+            segments[5] = SEGMENT_DIGITS[0];
+        } else {
+            segments[4] = SEGMENT_DIGITS[1];
+            segments[5] = SEGMENT_DIGITS[9];
+        }
+        segments[6] = SEGMENT_DIGITS[(state->time.year / 10) % 10];
+        segments[7] = SEGMENT_DIGITS[state->time.year % 10];
+        if (state->blink) {
+            switch (state->clock_selected_field) {
+                case SELECTED_DAY:
+                    segments[0] = 0;
+                    segments[1] = 0 | SEG_DOT;
+                    break;
+                case SELECTED_MONTH:
+                    segments[2] = 0;
+                    segments[3] = 0 | SEG_DOT;
+                    break;
+                case SELECTED_YEAR:
+                    segments[4] = 0;
+                    segments[5] = 0;
+                    segments[6] = 0;
+                    segments[7] = 0;
+                    break;
+            }
+        }
+    }
+    uint8_t weekday = state->time.weekday;
+    if (state->blink && state->clock_selected_field == SELECTED_WEEKDAY) {
+        weekday = 0;
+    }
+
+    LEDKey.set_display_data(weekday, segments);
 }
 
 void loop_once(struct State *state) {
