e7efa6b24b4382b0663d460c73b1cea982c586a2
Simon Struck <simon@simonscode.org>
2021-11-25 20:17:30 +0100

    Fix GPIO function readability

--- Mct_B4/src/GPIO.c
+++ Mct_B4/src/GPIO.c
@@ -13,14 +13,14 @@ static void set_pin_sel(const struct GPIOPin *pin) {
 }
 
 static void set_pin_mode(const struct GPIOPin *pin) {
-    volatile uint32_t *pinsel = &LPC_PINCON->PINMODE0;
-    pinsel += pin->port * 2;
+    volatile uint32_t *pinmode = &LPC_PINCON->PINMODE0;
+    pinmode += pin->port * 2;
 
     if (pin->pin < 16) {
-        *pinsel |= (pin->mode << (pin->pin * 2));
+        *pinmode |= (pin->mode << (pin->pin * 2));
     } else {
-        *pinsel += 1;
-        *pinsel |= (pin->mode << ((pin->pin - 16) * 2));
+        *pinmode += 1;
+        *pinmode |= (pin->mode << ((pin->pin - 16) * 2));
     }
 }
 
@@ -31,7 +31,7 @@ static void set_pin_open_drain(const struct GPIOPin *pin) {
     *pinsel |= (pin->open_drain << (pin->pin));
 }
 
-static LPC_GPIO_TypeDef *port_to_gpio_address(uint8_t port) {
+static LPC_GPIO_TypeDef *get_gpio_port(uint8_t port) {
     LPC_GPIO_TypeDef *ptr = (LPC_GPIO_TypeDef *) LPC_GPIO_BASE;
     ptr += port;
     return ptr;
@@ -43,7 +43,7 @@ static void init_pin(const struct GPIOPin *pin) {
     set_pin_mode(pin);
     set_pin_open_drain(pin);
 
-    volatile LPC_GPIO_TypeDef *gpio = port_to_gpio_address(pin->port);
+    volatile LPC_GPIO_TypeDef *gpio = get_gpio_port(pin->port);
 
     if (pin->dir == OUTPUT) {
         gpio->FIODIR |= (1 << pin->pin);
@@ -54,15 +54,11 @@ static void init_pin(const struct GPIOPin *pin) {
 }
 
 static void set_high(const struct GPIOPin *pin) {
-    volatile LPC_GPIO_TypeDef *gpio = port_to_gpio_address(pin->port);
-
-    gpio->FIOSET = (1 << pin->pin);
+    get_gpio_port(pin->port)->FIOSET = (1 << pin->pin);
 }
 
 static void set_low(const struct GPIOPin *pin) {
-    volatile LPC_GPIO_TypeDef *gpio = port_to_gpio_address(pin->port);
-
-    gpio->FIOCLR = (1 << pin->pin);
+    get_gpio_port(pin->port)->FIOCLR = (1 << pin->pin);
 }
 
 static void set(const struct GPIOPin *pin, bool state) {
@@ -74,9 +70,9 @@ static void set(const struct GPIOPin *pin, bool state) {
 }
 
 static bool get(const struct GPIOPin *pin) {
-    volatile LPC_GPIO_TypeDef *gpio = port_to_gpio_address(pin->port);
-
-    return (gpio->FIOPIN & (1 << pin->pin)) != 0;
+    volatile uint32_t fiopin = get_gpio_port(pin->port)->FIOPIN;
+    uint32_t masked_pin = fiopin & (1 << pin->pin);
+    return masked_pin != 0;
 }
 
 const struct gpio GPIO = {
