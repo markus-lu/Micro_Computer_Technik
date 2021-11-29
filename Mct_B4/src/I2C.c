cb36ed55cf43d64f616199c55471f13ef07e3148
Simon Struck <simon@simonscode.org>
2021-11-29 10:35:13 +0100

    Fix I2C Address shift

--- Mct_B4/src/I2C.c
+++ Mct_B4/src/I2C.c
@@ -81,7 +81,7 @@ welche es erlaubt ein Array an Bytes mit einem Funktionsaufruf zu lesen.
 **********************************************************************/
 uint8_t i2c_read(uint8_t address, uint8_t *data, uint32_t length) {
     i2c_start();
-    uint8_t status = i2c_write_byte(address | I2C_READ);
+    uint8_t status = i2c_write_byte((address << 1) | I2C_READ);
     if (status == ACK) {
         for (uint32_t i = 0; i < length; ++i) {
             // discard result, it's also not checked in the lcd library
