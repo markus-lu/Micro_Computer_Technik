84e1c23e872cf2ab8650439a7f63b8b937b5903c
Simon Struck <simon@simonscode.org>
2021-12-01 17:46:57 +0100

    Doxygen: Timer

--- Mct_B4/inc/Timer.h
+++ Mct_B4/inc/Timer.h
@@ -14,34 +14,82 @@
 #include "types.h"
 #include "LPC17xx.h"
 
-// Power Control for Peripherals register
+/**
+ *  \file    Timer.h
+*/
+
+/**
+ * \brief Power Control for Peripherals Register für Stromzufuhr von Timer 1
+ */
 #define TIMER1_POWER_CONTROL_PERIPHERALS_BIT (1 << 2)
+/**
+ * \brief Power Control for Peripherals Register für Stromzufuhr von Timer 2
+ */
 #define TIMER2_POWER_CONTROL_PERIPHERALS_BIT (1 << 22)
 
 
+
+/**
+ * \brief Clock Divider Register Teilung durch 1 für Timer
+ */
 #define CCLK_DIVIDED_BY_1 0b01
+/**
+ * \brief Clock Divider Register Teilung durch 2 für Timer
+ */
 #define CCLK_DIVIDED_BY_2 0b10
+/**
+ * \brief Clock Divider Register Teilung durch 4 für Timer
+ */
 #define CCLK_DIVIDED_BY_4 0b00
+/**
+ * \brief Clock Divider Register Teilung durch 8 für Timer
+ */
 #define CCLK_DIVIDED_BY_8 0b11
+
+
+/**
+ * \brief Clock Divider Register Register für Clock-Teilung von Timer 1
+ */
 #define TIMER1_PCLKSEL_BIT(value) (value << 4)
+/**
+ * \brief Clock Divider Register Register für Clock-Teilung von Timer 2
+ */
 #define TIMER2_PCLKSEL_BIT(value) (value << 12)
 
+
 // TCR
+/**
+ * \brief Timer Control Register Bit zum Aktivieren vom Timer
+ */
 #define TIMER_ENABLE_BIT (1 << 0)
+/**
+ * \brief Timer Control Register Bit zum Rücksetzen vom Timer
+ */
 #define TIMER_RESET_BIT (1 << 1)
 
+
 // MCR
+/**
+ * \brief Match Control Register Interrupt Bit
+ */
 #define INTERRUPT_ON_MATCH_BIT 0b001
+/**
+ * \brief Match Control Register Reset Bit
+ */
 #define RESET_ON_MATCH_BIT     0b010
+/**
+ * \brief Match Control Register Stop Bit
+ */
 #define STOP_ON_MATCH_BIT      0b100
 
+/**
+ * \brief Match Register Breite pro Match
+ */
 #define MATCH_CONTROL_REGISTER_WIDTH 3
 
 void timer_init_timer1(void);
 
 void timer_init_timer2(void);
-
-uint32_t timer_get_count(LPC_TIM_TypeDef *timer);
 
 void timer_set_prescaler(LPC_TIM_TypeDef *timer, uint32_t value);
 
--- Mct_B4/src/Timer.c
+++ Mct_B4/src/Timer.c
@@ -1,8 +1,38 @@
 #include "Timer.h"
 
+/**
+ *  \file    Timer.c
+*/
+
+/**
+ * \brief Globale Variable um mit dem ISR von Timer 1 zu kommunizieren.
+ */
 static volatile bool timer1_ticked;
+
+/**
+ * \brief Globale Variable um mit dem ISR von Timer 2 zu kommunizieren.
+ */
 static volatile bool timer2_ticked;
 
+/*********************************************************************/
+/**
+\brief Diese Funktion initialisiert Timer 1.
+
+ 1. Die Peripherie erhält Stromzufuhr durch das PCONP-Register
+ 2. Die Clock wird im PCLKSEL-Register gesetzt
+ 3. Timer wird zurückgesetzt
+ 4. Interrupt wird gecleared
+ 5. Interrupt wird aktiviert
+
+\param  -
+
+\return -
+
+\version 20.11.2021
+
+\todo -
+\bug  keine Fehler bekannt
+**********************************************************************/
 void timer_init_timer1 () {
 	// Power Contol for Peripherals Register
 	// Bit kontrolieren Stromzufuhr zur angeschlossenden Peripherie
@@ -22,6 +52,25 @@ void timer_init_timer1 () {
     NVIC_EnableIRQ(TIMER1_IRQn);
 }
 
+/*********************************************************************/
+/**
+\brief Diese Funktion initialisiert Timer 2.
+
+ 1. Die Peripherie erhält Stromzufuhr durch das PCONP-Register
+ 2. Die Clock wird im PCLKSEL-Register gesetzt
+ 3. Timer wird zurückgesetzt
+ 4. Interrupt wird gecleared
+ 5. Interrupt wird aktiviert
+
+\param  -
+
+\return -
+
+\version 20.11.2021
+
+\todo -
+\bug  keine Fehler bekannt
+**********************************************************************/
 void timer_init_timer2 () {
 	// Power Contol for Peripherals Register
 	// Bit kontrolieren Stromzufuhr zur angeschlossenden Peripherie
@@ -41,16 +90,64 @@ void timer_init_timer2 () {
     NVIC_EnableIRQ(TIMER2_IRQn);
 }
 
+/*********************************************************************/
+/**
+\brief Diese Funktion startet einen Timer.
+
+\param  timer
+        Pointer zu dem Timer, der gestartet werden soll
+        (LPC_TIM0, LPC_TIM1, LPC_TIM2, LPC_TIM3)
+
+\return -
+
+\version 20.11.2021
+
+\todo -
+\bug  keine Fehler bekannt
+**********************************************************************/
 void timer_start_timer (LPC_TIM_TypeDef *timer) {
 	// Timer Control Register
 	// Enable Bit Setzen um den Timer zu starten
     timer->TCR |= TIMER_ENABLE_BIT;
 }
 
+/*********************************************************************/
+/**
+\brief Diese Funktion stoppt einen Timer.
+
+\param  timer
+        Pointer zu dem Timer, der gestoppt werden soll
+        (LPC_TIM0, LPC_TIM1, LPC_TIM2, LPC_TIM3)
+
+\return -
+
+\version 20.11.2021
+
+\todo -
+\bug  keine Fehler bekannt
+**********************************************************************/
 void timer_stop_timer (LPC_TIM_TypeDef *timer) {
     timer->TCR &= ~TIMER_ENABLE_BIT;
 }
 
+/*********************************************************************/
+/**
+\brief Diese Funktion setzt den Prescaler für einen Timer.
+
+\param  timer
+        Pointer zu dem Timer, dessen Prescaler gesetzt werden soll
+        (LPC_TIM0, LPC_TIM1, LPC_TIM2, LPC_TIM3)
+\param  value
+        Wert, auf den der Prescaler gesetzt werden soll.
+        (0 - 4294967295)
+
+\return -
+
+\version 20.11.2021
+
+\todo -
+\bug  keine Fehler bekannt
+**********************************************************************/
 void timer_set_prescaler (LPC_TIM_TypeDef *timer, uint32_t value) {
 	// Prescale-Register
 	// Wert setzen
@@ -58,6 +155,27 @@ void timer_set_prescaler (LPC_TIM_TypeDef *timer, uint32_t value) {
     timer->PR = value;
 }
 
+/*********************************************************************/
+/**
+\brief Diese Funktion aktiviert uns setzt einen Matchwert für einen Timer.
+
+\param  timer
+        Pointer zu dem Timer, dessen Match aktiviert werden soll
+        (LPC_TIM0, LPC_TIM1, LPC_TIM2, LPC_TIM3)
+\param  match
+        Match-Register
+        (0 - 3)
+\param  value
+        Wert, auf den das Match-Register gesetzt werden soll.
+        (0 - 4294967295)
+
+\return -
+
+\version 20.11.2021
+
+\todo -
+\bug  keine Fehler bekannt
+**********************************************************************/
 void timer_enable_match_interrupt (LPC_TIM_TypeDef *timer, uint8_t match, uint32_t value) {
 	// Match Control Register
 	// Bei einem Match soll der Timer einnen Interupt auslösen und wider von neuem anfangen zu zählen
@@ -73,10 +191,44 @@ void timer_enable_match_interrupt (LPC_TIM_TypeDef *timer, uint8_t match, uint32
     *match_register = value;
 }
 
+/*********************************************************************/
+/**
+\brief Diese Funktion cleared das Interrupt-Register für ein Match für einen Timer.
+
+\param  timer
+        Pointer zu dem Timer, dessen Match gecleared werden soll
+        (LPC_TIM0, LPC_TIM1, LPC_TIM2, LPC_TIM3)
+\param  match
+        Match-Register
+        (0 - 3)
+
+\return -
+
+\version 20.11.2021
+
+\todo -
+\bug  keine Fehler bekannt
+**********************************************************************/
 void timer_clear_match_interrupt (LPC_TIM_TypeDef *timer, uint8_t match) {
     timer->IR |= (1 << match);
 }
 
+/*********************************************************************/
+/**
+\brief Diese Funktion prüft, ob Timer 1 seit dem letzten Funktionsaufruf einen Interrupt hatte (getickt hat)
+
+ Prüft und aktualisiert globale Variable timer2_ticked.
+
+\param  -
+
+\return Boolean, der angibt, ob Timer 1 getickt hat
+        (false / true)
+
+\version 20.11.2021
+
+\todo -
+\bug  keine Fehler bekannt
+**********************************************************************/
 bool timer_has_timer1_ticked () {
 	// Merken ob Timer1 getickt hat
     bool has_ticked = timer1_ticked;
@@ -86,17 +238,62 @@ bool timer_has_timer1_ticked () {
     return has_ticked;
 }
 
+
+/*********************************************************************/
+/**
+\brief Diese Funktion prüft, ob Timer 2 seit dem letzten Funktionsaufruf einen Interrupt hatte (getickt hat)
+
+ Prüft und aktualisiert globale Variable timer2_ticked.
+\param  -
+
+\return Boolean, der angibt, ob Timer 2 getickt hat
+        (false / true)
+
+\version 20.11.2021
+
+\todo -
+\bug  keine Fehler bekannt
+**********************************************************************/
 bool timer_has_timer2_ticked () {
     bool has_ticked = timer2_ticked;
     timer2_ticked = false;
     return has_ticked;
 }
 
+
+/*********************************************************************/
+/**
+\brief Diese Funktion deinitialisiert Timer 1.
+
+\param  -
+
+\return -
+
+\version 20.11.2021
+
+\todo -
+\bug  keine Fehler bekannt
+**********************************************************************/
 void timer_deinit_timer1 () {
     timer_stop_timer(LPC_TIM1);
     LPC_TIM1->TCR = TIMER_RESET_BIT;
+    NVIC_DisableIRQ(TIMER1_IRQn);
+    NVIC_ClearPendingIRQ(TIMER1_IRQn);
 }
 
+/*********************************************************************/
+/**
+\brief Diese Funktion deinitialisiert Timer 2.
+
+\param  -
+
+\return -
+
+\version 20.11.2021
+
+\todo -
+\bug  keine Fehler bekannt
+**********************************************************************/
 void timer_deinit_timer2 () {
     timer_stop_timer(LPC_TIM2);
     LPC_TIM2->TCR = TIMER_RESET_BIT;
@@ -104,12 +301,43 @@ void timer_deinit_timer2 () {
     NVIC_ClearPendingIRQ(TIMER2_IRQn);
 }
 
+/*********************************************************************/
+/**
+\brief Interrupt Service Routine für Timer 1.
+
+ Setzt globale Variable timer1_ticked auf true.
+
+\param  -
+
+\return -
+
+\version 20.11.2021
+
+\todo -
+\bug  keine Fehler bekannt
+**********************************************************************/
 void TIMER1_IRQHandler () {
     NVIC_ClearPendingIRQ(TIMER1_IRQn);
     timer_clear_match_interrupt(LPC_TIM1, 1);
     timer1_ticked = true;
 }
 
+
+/*********************************************************************/
+/**
+\brief Interrupt Service Routine für Timer 2.
+
+ Setzt globale Variable timer2_ticked auf true.
+
+\param  -
+
+\return -
+
+\version 20.11.2021
+
+\todo -
+\bug  keine Fehler bekannt
+**********************************************************************/
 void TIMER2_IRQHandler () {
     NVIC_ClearPendingIRQ(TIMER2_IRQn);
     timer_clear_match_interrupt(LPC_TIM2, 1);
