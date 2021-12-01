6559ddd7a9ee9af19a6a2c6510d33e910ccf0af4
Simon Struck <simon@simonscode.org>
2021-12-01 16:29:48 +0100

    Doxygen: I2C

--- Mct_B4/inc/I2C.h
+++ Mct_B4/inc/I2C.h
@@ -14,15 +14,33 @@
 #include "types.h"
 #include "i2c_1769.h"
 
-// Portexpander für 16 rote LEDs
+/**
+ *  \file     I2C.h
+*/
+
+/**
+ * \brief Portexpander für 16 rote LEDs
+ */
 #define PCA9539_ADDRESS 0x74
-// Portexpander Frontplatte
+
+/**
+ * \brief Portexpander Frontplatte
+ */
 #define PCF8574A_ADDRESS 0x3F
-// Portexpander für das LCD
+
+/**
+ * \brief Portexpander für das LCD
+ */
 #define PCF8574_LCD_ADDRESS 0x27
-// RTC Modul
+
+/**
+ * \brief RTC Modul DS3231
+ */
 #define DS3231_ADDRESS 0x68
 
+/**
+ * \brief I²C Geschwindigkeit
+ */
 #define I2C_SPEED I2C_FM
 
 uint8_t i2c_write(uint8_t address, uint8_t *data, uint32_t length);
