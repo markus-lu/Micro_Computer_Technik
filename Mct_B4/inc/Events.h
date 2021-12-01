9aa813e68253eb14d9794e8c62f1ff4edaf74272
Markus Luckau <markus.luckau@gmail.com>
2021-12-01 15:55:04 +0100

    Doxygen: Events

--- Mct_B4/inc/Events.h
+++ Mct_B4/inc/Events.h
@@ -13,23 +13,87 @@
 
 #include "types.h"
 
+/**
+ *  \file    Events.h
+*/
 
+/**
+ * Anzahl der möglichen Events
+ */
 #define EVENT_COUNT 16
 
+/**
+ * Bitcodieung für Montag
+ */
 #define Monday (1 << 6)
+
+/**
+ * Bitcodieung für Dienstag
+ */
 #define Tuesday (1 << 5)
+
+/**
+ * Bitcodieung für Mitwoch
+ */
 #define Wednesday (1 << 4)
+
+/**
+ * Bitcodieung für Donerstag
+ */
 #define Thursday (1 << 3)
+
+/**
+ * Bitcodieung für Freitag
+ */
 #define Friday (1 << 2)
+
+/**
+ * Bitcodieung für Samstag
+ */
 #define Saturday (1 << 1)
+
+/**
+ * Bitcodieung für Sontag
+ */
 #define Sunday (1 << 0)
 
+
+/**
+ * Ein Event Struct in welchem alle wichtigen Daten für ein Event gespeichert wird.
+ */
 struct Event {
+	/**
+	 * \brief Bool der anzeigt ob ein Event aktiviert (true) oder deaktiviert (false) ist.
+	 *
+	 * Wertebereich: false / true
+	 */
     bool enabled;
+    /**
+     * \brief Integer der den Stundenanteil des Events enthält
+     *
+     * Wertebereich: 0 - 23
+     */
     uint8_t hour;
+    /**
+	 * \brief Integer der den Minutenanteil des Events enthält
+	 *
+	 * Wertebereich: 0 - 59
+	 */
     uint8_t minute;
-    uint8_t weekdays; // MSB = 0, Monday, ..., LSB = Sunday
-    bool on_or_off; // 1 = on, 0 = off
+    /**
+	 * \brief Integer der den Wochentag des Events enthält
+	 * MSB = Monday, ..., Sunday, LSB = 0
+	 *
+	 * Wertebereich: 0x80 - 0x02
+	 */
+    uint8_t weekdays;
+    /**
+     * \brief Bool der anzeigt ob die RGB_LED rot (off) oder grün (on) leuchten soll
+     * true = on, false = off
+     *
+     * Wertebereich:  false / true
+     */
+    bool on_or_off;
 };
 
 void events_init(struct Event *event_data);
--- Mct_B4/src/Events.c
+++ Mct_B4/src/Events.c
@@ -1,5 +1,26 @@
 #include "Events.h"
 
+/**
+ *  \file    Events.c
+*/
+
+
+/*********************************************************************/
+ /**
+ Diese Funktion füllt ein Event Struct Array mit Startwerten .
+
+ \param	 event_data
+ 	 	 Pointer der ein Event Struct Array enthält. In ihm werden alle wichtigen Daten
+ 	 	 für die Events gespeichert
+ 	 	 (Wertebereich in der Struct-Definition)
+
+ \return  -
+
+ \version 30.11.2021
+
+ \todo -
+ \bug  keine Fehler bekannt
+ **********************************************************************/
 void events_init(struct Event *event_data) {
 	// Schleife die Startwerte für die Events Festsetzen
     for (int i = 0; i < EVENT_COUNT; ++i) {
@@ -11,6 +32,24 @@ void events_init(struct Event *event_data) {
     }
 }
 
+/*********************************************************************/
+ /**
+ Diese Funktion zählt alle abgespeicherten und aktivierten Events.
+
+ \param	 event_data
+ 	 	 Pointer der ein Event Struct Array enthält. In ihm werden alle wichtigen Daten
+ 	 	 für die Events gespeichert.
+ 	 	 (Wertebereich in der Struct-Definition)
+
+ \return count
+ 	 	 Integer der die Anzahl der abgespeicherten und aktivierten Events enthält.
+
+
+ \version 30.11.2021
+
+ \todo -
+ \bug  keine Fehler bekannt
+ **********************************************************************/
 uint16_t events_get_count(const struct Event *event_data) {
 	// Zähler erstellen
     uint16_t count = 0;
